9d519ddff30dac74d829f7bc8ed2fda909619ad6
LiaVernaet <l_vernaet@fanshaweonline.ca>
2019-03-14 17:19:58 -0400

    updated

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page. Goes into body tag */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Your First Express App', message: 'Express is actually pretty easy once you figure it out' });
 });
 
 module.exports = router;
